40a9bb073f78603628e329c392da380ca3bf6ab8
Maciej Grula <maciej.grula@execon.pl>
2025-07-17 12:58:24 +0200

    exporting

--- src/q247-models-core.ts
+++ src/q247-models-core.ts
@@ -1,5 +1,5 @@
 /* istanbul ignore file */
-export type {ScalarScore, Score, ScoreModel, ScoreModelCard, ContentScoreModel} from "./iface/model"
-export type {Event, Decoded} from "./iface/event"
+export type {ScalarScore, Score, ScoreModel, ScoreModelCard, ContentScoreModel, ScalarScoreWithChangeSummary, ModelParams} from "./iface/model"
+export type {Event, Decoded, Author, ChangeSummary, DecodedBase, EventBase} from "./iface/event"
 
 export type {Content} from "./iface/content"
