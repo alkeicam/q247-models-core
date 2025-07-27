e3a8208e37843eb8bd7c424b34864afd13b9acf1
Maciej Grula <maciej.grula@execon.pl>
2025-07-27 18:19:26 +0200

    fix

--- src/q247-models-core.ts
+++ src/q247-models-core.ts
@@ -1,7 +1,7 @@
 /* istanbul ignore file */
-export type {ScalarScore, Score, ScoreModel, ScoreModelCard, ContentScoreModel, ScalarScoreWithChangeSummary, ModelParams, HalsteadScore} from "./iface/model"
+export type {ScalarScore, Score, ScoreModel, ScoreModelCard, ContentScoreModel, ScalarScoreWithChangeSummary, ModelParams} from "./iface/model"
 export type {Event, Decoded, Author, ChangeSummary, DecodedBase, EventBase} from "./iface/event"
-export {Diff} from "./iface/model"
+export {Diff, HalsteadScore} from "./iface/model"
 export type {Content} from "./iface/content"
 
 export {parseGitDiff} from "./logic/commons"
